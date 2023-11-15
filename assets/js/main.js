56ba125387b7c992ab1c4e22bfa9b7bc9e808887
mathieu <degueurce.mathieu@gmail.com>
2023-11-15 19:18:22 +0100

    Ajout input invisible

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,11 @@
-window.onload = setInterval(cmdPrompt, 500);
+let input = document.getElementById('cmd');
+input.value = '';
+input.focus();
 
-function cmdPrompt() {
-    const prompt = document.getElementById("prompt");
-    if (prompt.style.display === "none") {
-        prompt.style.display = "inline";
-    } else {
-        prompt.style.display = "none";
-    }
+function ajusterTaille(input) {
+    // Récupérer la longueur du texte
+    let longueurTexte = input.value.length;
+
+    // Ajuster la largeur de l'input en fonction de la longueur du texte
+    input.style.width = (longueurTexte * 10) + 'px'; // Vous pouvez ajuster le facteur multiplicatif selon vos besoins
 }
