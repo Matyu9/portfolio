ec8ad6b1c89083e200fa6a27de60f615bc432bf4
mathieu <degueurce.mathieu@gmail.com>
2023-11-15 21:29:18 +0100

    prmière commande (fin presque)

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,24 @@
 const input = document.getElementById('cmd');
-const fieldToUpdate = document.getElementById('cmd-results-input')
+const fieldToUpdate = document.getElementById('cmd-results-input');
+let commands = [];
 input.value = '';
 input.focus();
+
+
+fetch('assets/js/json/cmd.json')
+    .then(response => {
+        if (!response.ok){
+            throw new Error('Erreur de chargements du fichier json');
+        }
+        return response.json();
+    })
+    .then(data =>{
+        commands = data;
+    })
+    .catch(error => {
+        console.log('Une erreur est survenue ! ', error)
+    });
+
 
 function ajusterTaille(input) {
     // Récupérer la longueur du texte
@@ -12,7 +29,25 @@ function ajusterTaille(input) {
 }
 
 function ajouterCommande(cmd){
+    const element = document.createElement('div');
 
+    const cmdName = document.createElement('p');
+    cmdName.setAttribute('class', 'command');
+    cmdName.innerHTML = `<span class="green">matyu@deb:~$</span> ${cmd}`;
+
+    const cmdResult = document.createElement('p');
+    cmdResult.setAttribute('class', 'result')
+    try {
+        cmdName.innerHTML = commands.cmd;
+    } catch (error) {
+
+    }
+
+    element.appendChild(cmdName);
+
+    fieldToUpdate.appendChild(element);
+    input.scrollIntoView({ behavior: 'smooth', block: 'end' });
+    window.scrollBy(0, 1000);
 }
 
 input.addEventListener('keydown', (event) => {
