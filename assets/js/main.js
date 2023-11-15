921e008e6a1cd133b7bec6cd862c9ff790abca9d
mathieu <degueurce.mathieu@gmail.com>
2023-11-15 19:45:30 +0100

    trop de modif mais en gros debut js des commandes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,5 @@
-let input = document.getElementById('cmd');
+const input = document.getElementById('cmd');
+const fieldToUpdate = document.getElementById('cmd-results-input')
 input.value = '';
 input.focus();
 
@@ -9,3 +10,14 @@ function ajusterTaille(input) {
     // Ajuster la largeur de l'input en fonction de la longueur du texte
     input.style.width = (longueurTexte * 10) + 'px'; // Vous pouvez ajuster le facteur multiplicatif selon vos besoins
 }
+
+function ajouterCommande(cmd){
+
+}
+
+input.addEventListener('keydown', (event) => {
+    if (event.key === 'Enter') {
+        ajouterCommande(input.value)
+        input.value = '';
+    }
+});
