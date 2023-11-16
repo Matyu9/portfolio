c0ce2ffcca530e92a1a20d67143608c5ebe72ce6
mathieu <degueurce.mathieu@gmail.com>
2023-11-16 20:15:48 +0100

    fix si 1 caractère puis plus rien placeholder visible

--- assets/js/main.js
+++ assets/js/main.js
@@ -23,8 +23,10 @@ function ajusterTaille(input) {
     // Récupérer la longueur du texte
     let longueurTexte = input.value.length;
 
-    // Ajuster la largeur de l'input en fonction de la longueur du texte
-    input.style.width = (longueurTexte * 10) + 'px'; // Vous pouvez ajuster le facteur multiplicatif selon vos besoins
+    if (longueurTexte * 10 >= input.style.width) {
+        // Ajuster la largeur de l'input en fonction de la longueur du texte
+        input.style.width = (longueurTexte * 10) + 'px'; // Vous pouvez ajuster le facteur multiplicatif selon vos besoins
+    } else {}
 }
 
 function ajouterCommande(cmd){
